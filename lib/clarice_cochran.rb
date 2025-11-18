2eedd770ce7e2634b177e3bfc3d5f93559270b34
ohataken <11092425+ohataken@users.noreply.github.com>
2025-11-18 15:20:52 +0900

    chore: require json

--- lib/clarice_cochran.rb
+++ lib/clarice_cochran.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "json"
 require_relative "clarice_cochran/version"
 require_relative "clarice_cochran/session_start_hook_command_builder"
 require_relative "clarice_cochran/session_end_hook_command_builder"
