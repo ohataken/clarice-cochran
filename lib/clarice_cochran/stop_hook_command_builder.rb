da1db00c509f2feaec35fccc321df01566bfea91
ohataken <11092425+ohataken@users.noreply.github.com>
2025-11-18 14:40:41 +0900

    feat: update clarice-cochran-stop-hook

--- lib/clarice_cochran/stop_hook_command_builder.rb
+++ lib/clarice_cochran/stop_hook_command_builder.rb
@@ -2,5 +2,8 @@
 
 module ClariceCochran
   class StopHookCommandBuilder
+    def to_osascript
+      'osascript -e \'display notification "処理が停止しました" with title "Claude Code" subtitle "停止完了" sound name "Hero"\''
+    end
   end
 end
