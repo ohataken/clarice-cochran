60b3bd1c0cdb0f25db6a2987a910f0bafa136888
ohataken <11092425+ohataken@users.noreply.github.com>
2025-11-18 14:40:15 +0900

    feat: update clarice-cochran-session-end-hook

--- lib/clarice_cochran/session_end_hook_command_builder.rb
+++ lib/clarice_cochran/session_end_hook_command_builder.rb
@@ -2,5 +2,8 @@
 
 module ClariceCochran
   class SessionEndHookCommandBuilder
+    def to_osascript
+      'osascript -e \'display notification "セッションが終了しました" with title "Claude Code" subtitle "処理終了" sound name "Hero"\''
+    end
   end
 end
