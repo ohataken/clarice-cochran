1119a78ebb0921600598a2d385b4fb9e4f3b0f63
ohataken <11092425+ohataken@users.noreply.github.com>
2025-11-18 15:07:16 +0900

    feat: update clarice-cochran-notification-hook

--- lib/clarice_cochran/notification_hook_command_builder.rb
+++ lib/clarice_cochran/notification_hook_command_builder.rb
@@ -2,6 +2,10 @@
 
 module ClariceCochran
   class NotificationHookCommandBuilder
+    def initialize(data)
+      @data = data
+    end
+
     def to_osascript
       'osascript -e \'display notification "通知があります" with title "Claude Code" subtitle "お知らせ" sound name "Hero"\''
     end
