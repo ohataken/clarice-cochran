61e909f194051553e500f7d8e04d989e8e21aac9
ohataken <11092425+ohataken@users.noreply.github.com>
2025-11-18 13:15:52 +0900

    chore: initial commit

---
+++ lib/clarice_cochran.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "clarice_cochran/version"
+
+module ClariceCochran
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "clarice_cochran"
+
+require "minitest/autorun"
