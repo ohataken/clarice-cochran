b78efda53ac840387ebe4893d74079348f9bc59c
ohataken <11092425+ohataken@users.noreply.github.com>
2025-11-18 14:26:37 +0900

    chore: add NotificationHookCommandBuilder

--- lib/clarice_cochran.rb
+++ lib/clarice_cochran.rb
@@ -3,6 +3,7 @@
 require_relative "clarice_cochran/version"
 require_relative "clarice_cochran/session_start_hook_command_builder"
 require_relative "clarice_cochran/session_end_hook_command_builder"
+require_relative "clarice_cochran/notification_hook_command_builder"
 
 module ClariceCochran
   class Error < StandardError; end
