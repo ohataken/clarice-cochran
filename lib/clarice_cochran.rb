3b4452a22d2c287204772b072139c33ab594ecb0
ohataken <11092425+ohataken@users.noreply.github.com>
2025-11-18 14:26:49 +0900

    chore: add StopHookCommandBuilder

--- lib/clarice_cochran.rb
+++ lib/clarice_cochran.rb
@@ -4,6 +4,7 @@ require_relative "clarice_cochran/version"
 require_relative "clarice_cochran/session_start_hook_command_builder"
 require_relative "clarice_cochran/session_end_hook_command_builder"
 require_relative "clarice_cochran/notification_hook_command_builder"
+require_relative "clarice_cochran/stop_hook_command_builder"
 
 module ClariceCochran
   class Error < StandardError; end
---
+++ lib/clarice_cochran/stop_hook_command_builder.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module ClariceCochran
+  class StopHookCommandBuilder
+  end
+end
