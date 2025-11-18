15e8f712d27f8f894024c2b4b673f6cbab1c4385
ohataken <11092425+ohataken@users.noreply.github.com>
2025-11-18 14:39:59 +0900

    feat: update clarice-cochran-session-start-hook

--- lib/clarice_cochran/session_start_hook_command_builder.rb
+++ lib/clarice_cochran/session_start_hook_command_builder.rb
@@ -2,5 +2,8 @@
 
 module ClariceCochran
   class SessionStartHookCommandBuilder
+    def to_osascript
+      'osascript -e \'display notification "セッションが開始されました" with title "Claude Code" subtitle "処理開始" sound name "Hero"\''
+    end
   end
 end
