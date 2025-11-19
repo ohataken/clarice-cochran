ad4348e0d19578460e587d8b729e239df5a1e08a
ohataken <11092425+ohataken@users.noreply.github.com>
2025-11-19 21:24:24 +0900

    chore: add TranscriptFileParser#assistant_transcripts_with_messages

--- lib/clarice_cochran/transcript_file_parser.rb
+++ lib/clarice_cochran/transcript_file_parser.rb
@@ -17,5 +17,11 @@ module ClariceCochran
     def parse
       @objects ||= lines.map { |l| TranscriptParser.new(JSON.parse(l)) }
     end
+
+    def assistant_transcripts_with_messages
+      parse.filter do |transcript|
+        transcript.type_assistant? && transcript.message_contents.any?
+      end
+    end
   end
 end
