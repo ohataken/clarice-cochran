d6022b17f269ad31fc98a5f4c2e3d56f964c9f80
ohataken <11092425+ohataken@users.noreply.github.com>
2025-11-21 10:19:54 +0900

    fix

--- lib/clarice_cochran/user_prompt_submit_hook_command_builder.rb
+++ lib/clarice_cochran/user_prompt_submit_hook_command_builder.rb
@@ -9,18 +9,7 @@ module ClariceCochran
     end
 
     def to_osascript
-      "osascript -e 'display notification \"#{latest_message_content_text}\" with title \"#{hook_event_name} - Claude Code\" subtitle \"#{notification_type}\" sound name \"Tink\"'"
-    end
-
-    private
-
-    def latest_transcript_with_message
-      transcript_file_parser.latest_assistant_transcript_with_message
-    end
-
-    def latest_message_content_text
-      transcript = latest_transcript_with_message
-      transcript.message_contents.last.message
+      "osascript -e 'display notification \"#{message}\" with title \"#{hook_event_name} - Claude Code\" subtitle \"#{notification_type}\" sound name \"Tink\"'"
     end
   end
 end
