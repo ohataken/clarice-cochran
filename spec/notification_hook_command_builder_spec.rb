7ce169bb6478c462d56ff3a459c95065a255c4ea
ohataken <11092425+ohataken@users.noreply.github.com>
2025-11-22 22:35:25 +0900

    test: update spec files (#32)

--- spec/notification_hook_command_builder_spec.rb
+++ spec/notification_hook_command_builder_spec.rb
@@ -3,6 +3,38 @@
 require "spec_helper"
 
 RSpec.describe ClariceCochran::NotificationHookCommandBuilder do
+  describe "#session_id" do
+    it "returns the session_id value" do
+      data = JSON.parse('{ "session_id": "12345" }')
+      builder = ClariceCochran::NotificationHookCommandBuilder.new(data)
+      expect(builder.session_id).to eq("12345")
+    end
+  end
+
+  describe "#transcript_path" do
+    it "returns the transcript_path value" do
+      data = JSON.parse('{ "transcript_path": "/path/to/transcript.jsonl" }')
+      builder = ClariceCochran::NotificationHookCommandBuilder.new(data)
+      expect(builder.transcript_path).to eq("/path/to/transcript.jsonl")
+    end
+  end
+
+  describe "#current_working_directory" do
+    it "returns the cwd value" do
+      data = JSON.parse('{ "cwd": "/Users/testuser/project" }')
+      builder = ClariceCochran::NotificationHookCommandBuilder.new(data)
+      expect(builder.current_working_directory).to eq("/Users/testuser/project")
+    end
+  end
+
+  describe "#hook_event_name" do
+    it "returns the hook_event_name value" do
+      data = JSON.parse('{ "hook_event_name": "Notification" }')
+      builder = ClariceCochran::NotificationHookCommandBuilder.new(data)
+      expect(builder.hook_event_name).to eq("Notification")
+    end
+  end
+
   describe "#message" do
     it "returns the message value" do
       data = JSON.parse('{ "message": "hi" }')
@@ -14,6 +46,14 @@ RSpec.describe ClariceCochran::NotificationHookCommandBuilder do
       data = JSON.parse("{}")
       builder = ClariceCochran::NotificationHookCommandBuilder.new(data)
       expect(builder.message).to eq("")
+    end
+  end
+
+  describe "#notification_type" do
+    it "returns the notification_type value" do
+      data = JSON.parse('{ "notification_type": "permission_prompt" }')
+      builder = ClariceCochran::NotificationHookCommandBuilder.new(data)
+      expect(builder.notification_type).to eq("permission_prompt")
     end
   end
 
--- spec/session_end_hook_command_builder_spec.rb
+++ spec/session_end_hook_command_builder_spec.rb
@@ -3,6 +3,46 @@
 require "spec_helper"
 
 RSpec.describe ClariceCochran::SessionEndHookCommandBuilder do
+  describe "#session_id" do
+    it "returns the session_id value" do
+      data = JSON.parse('{ "session_id": "12345" }')
+      builder = ClariceCochran::SessionEndHookCommandBuilder.new(data)
+      expect(builder.session_id).to eq("12345")
+    end
+  end
+
+  describe "#transcript_path" do
+    it "returns the transcript_path value" do
+      data = JSON.parse('{ "transcript_path": "/path/to/transcript.jsonl" }')
+      builder = ClariceCochran::SessionEndHookCommandBuilder.new(data)
+      expect(builder.transcript_path).to eq("/path/to/transcript.jsonl")
+    end
+  end
+
+  describe "#current_working_directory" do
+    it "returns the cwd value" do
+      data = JSON.parse('{ "cwd": "/Users/testuser/project" }')
+      builder = ClariceCochran::SessionEndHookCommandBuilder.new(data)
+      expect(builder.current_working_directory).to eq("/Users/testuser/project")
+    end
+  end
+
+  describe "#hook_event_name" do
+    it "returns the hook_event_name value" do
+      data = JSON.parse('{ "hook_event_name": "SessionEnd" }')
+      builder = ClariceCochran::SessionEndHookCommandBuilder.new(data)
+      expect(builder.hook_event_name).to eq("SessionEnd")
+    end
+  end
+
+  describe "#reason" do
+    it "returns the reason value" do
+      data = JSON.parse('{ "reason": "prompt_input_exit" }')
+      builder = ClariceCochran::SessionEndHookCommandBuilder.new(data)
+      expect(builder.reason).to eq("prompt_input_exit")
+    end
+  end
+
   describe "#message" do
     it "returns the message value" do
       data = JSON.parse('{ "message": "hi" }')
